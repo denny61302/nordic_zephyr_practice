b19575355c924e4de6cb7f9bb0b14dda3b840e5e
denny61302 <denny61302@gmail.com>
2022-05-25 11:09:59 +0930

    Update main.c
    add timer

--- src/main.c
+++ src/main.c
@@ -8,21 +8,37 @@
 #include <device.h>
 #include <devicetree.h>
 
-#include <logging/log.h>
+// #include <logging/log.h>
 #include <dk_buttons_and_leds.h>
 
 #include "adxl345.h"
 
 #include "remote.h"
 
-#define LOG_MODULE_NAME app
-LOG_MODULE_REGISTER(LOG_MODULE_NAME);
+// #define LOG_MODULE_NAME app
+// LOG_MODULE_REGISTER(LOG_MODULE_NAME);
 #define RUN_STATUS_LED DK_LED1
 #define CONN_STATUS_LED DK_LED2
 #define RUN_LED_BLINK_INTERVAL 250
 
 static struct bt_conn *current_conn;
 bool isNotify = false;
+int counter = 0;
+
+// static struct k_work my_work;
+
+// static void my_work_fn(struct k_work *work)
+// {
+// 	LOG_ERR("Timer\n");
+// }
+
+void timer_fn(struct k_timer *dummy)
+{
+	// k_work_submit(&my_work);
+	counter++;
+}
+
+K_TIMER_DEFINE(my_timer, timer_fn, NULL);
 
 /* Declarations */
 void on_connected(struct bt_conn *conn, uint8_t err);
@@ -44,17 +60,17 @@ struct bt_remote_service_cb remote_service_callbacks = {
 void on_connected(struct bt_conn *conn, uint8_t err)
 {
 	if(err) {
-		LOG_ERR("connection err: %d", err);
+		printk("connection err: %d\n", err);
 		return;
 	}
-	LOG_INF("Connected.");
+	printk("Connected.\n");
 	current_conn = bt_conn_ref(conn);
 	dk_set_led_on(CONN_STATUS_LED);
 }
 
 void on_disconnected(struct bt_conn *conn, uint8_t reason)
 {
-	LOG_INF("Disconnected (reason: %d)", reason);
+	printk("Disconnected (reason: %d)\n", reason);
 	dk_set_led_off(CONN_STATUS_LED);
 	if(current_conn) {
 		bt_conn_unref(current_conn);
@@ -66,10 +82,10 @@ void on_notif_changed(enum bt_button_notifications_enabled status)
 {
     if (status == BT_BUTTON_NOTIFICATIONS_ENABLED) {
 		isNotify = true;
-        LOG_INF("Notifications enabled");
+        printk("Notifications enabled\n");
     } else {
 		isNotify = false;
-        LOG_INF("Notifications disabled");
+        printk("Notifications disabled\n");
     }
 }
 
@@ -79,8 +95,8 @@ void on_data_received(struct bt_conn *conn, const uint8_t *const data, uint16_t 
     memcpy(temp_str, data, len);
     temp_str[len] = 0x00;
 
-    LOG_INF("Received data on conn %p. Len: %d", (void *)conn, len);
-    LOG_INF("Data: %s", log_strdup(temp_str));
+    printk("Received data on conn %p. Len: %d\n", (void *)conn, len);
+    printk("Data: %s\n", temp_str);
 }
 
 void button_handler(uint32_t button_state, uint32_t has_changed)
@@ -106,11 +122,11 @@ void button_handler(uint32_t button_state, uint32_t has_changed)
 			default:
 				break;
 		}
-        LOG_INF("Button %d pressed.", button_pressed);       
+        printk("Button %d pressed.\n", button_pressed);       
 		set_button_value(button_pressed);
 		err = send_button_notification(current_conn, &button_pressed, 1);
 		if (err) {
-            LOG_ERR("Couldn't send notificaton. (err: %d)", err);
+            printk("Couldn't send notificaton. (err: %d)\n", err);
         }
     }
 }
@@ -121,11 +137,11 @@ static void configure_dk_buttons_leds(void)
     int err;
     err = dk_leds_init();
     if (err) {
-        LOG_ERR("Couldn't init LEDS (err %d)", err);
+        printk("Couldn't init LEDS (err %d)\n", err);
     }
     err = dk_buttons_init(button_handler);
     if (err) {
-        LOG_ERR("Couldn't init buttons (err %d)", err);
+        printk("Couldn't init buttons (err %d)\n", err);
     }
 }
 
@@ -138,47 +154,70 @@ void main(void)
 
 	struct adxl345_data adxl345_data;
 
-	LOG_INF("Hello World! %s\n", CONFIG_BOARD);
+	printk("Hello World! %s\n", CONFIG_BOARD);
 
 	configure_dk_buttons_leds();
 
 	err = bluetooth_init(&bluetooth_callbacks, &remote_service_callbacks);
     if (err) {
-        LOG_INF("Couldn't initialize Bluetooth. err: %d", err);
+        printk("Couldn't initialize Bluetooth. err: %d\n", err);
+		return;
     }
 
 	/* Get the binding of the I2C driver  */
 	const struct device *dev_i2c = device_get_binding(I2C0);
 	if (dev_i2c == NULL) {
-		LOG_INF("Could not find  %s!\n\r",I2C0);
+		printk("Could not find  %s!\n",I2C0);
 		return;
 	}
 
 	/* Setup the sensor */
 	ret = adxl345_init(dev_i2c);
 	if(ret != 0){
-        LOG_INF("Failed to init ADXL345");
+        printk("Failed to init ADXL345\n");
+		return;
     }
+
+	k_timer_init(&my_timer, timer_fn, NULL);
+	k_timer_start(&my_timer, K_NO_WAIT, K_MSEC(250));	
+	// k_work_init(&my_work, my_work_fn);
 
 	while (1) {
 
-		dk_set_led(RUN_STATUS_LED, (blink_status++)%2);		
+		if(counter != 0){
+			counter = 0;
+			dk_set_led(RUN_STATUS_LED, (blink_status++)%2);			
 
-		ret = readXYZ(dev_i2c, &adxl345_data);
-        if(ret != 0){
-            LOG_INF("Failed to read adxl345 data");
-        }
-		
-		if(isNotify){
-			err = send_adxl345_notification(current_conn, (uint8_t*)&adxl345_data, sizeof(adxl345_data));
-			if (err) {
-				LOG_ERR("Couldn't send notificaton. (err: %d)", err);
+			ret = readXYZ(dev_i2c, &adxl345_data);
+			if(ret != 0){
+			    printk("Failed to read adxl345 data\n");
 			}
+			
+			if(isNotify){
+				err = send_adxl345_notification(current_conn, (uint8_t*)&adxl345_data, sizeof(adxl345_data));
+				if (err) {
+					printk("Couldn't send notificaton. (err: %d)\n", err);
+				}
+			}
+			printk("ACC X : %d, Y: %d, Z: %d \r\n", adxl345_data.x, adxl345_data.y, adxl345_data.z); 
 		}
-		
-		//Print reading to console  
-		LOG_INF("ACC X : %d, Y: %d, Z: %d \r\n", adxl345_data.x, adxl345_data.y, adxl345_data.z);  
+		// dk_set_led(RUN_STATUS_LED, (blink_status++)%2);			
 
-		k_sleep(K_MSEC(RUN_LED_BLINK_INTERVAL));
+		// ret = readXYZ(dev_i2c, &adxl345_data);
+        // if(ret != 0){
+        //     LOG_INF("Failed to read adxl345 data");
+        // }
+		
+		// if(isNotify){
+		// 	err = send_adxl345_notification(current_conn, (uint8_t*)&adxl345_data, sizeof(adxl345_data));
+		// 	if (err) {
+		// 		LOG_ERR("Couldn't send notificaton. (err: %d)", err);
+		// 	}
+		// }
+		
+		// //Print reading to console  
+		// LOG_INF("ACC X : %d, Y: %d, Z: %d \r\n", adxl345_data.x, adxl345_data.y, adxl345_data.z);  
+
+		// k_sleep(K_MSEC(RUN_LED_BLINK_INTERVAL));
 	}	
 }
